fe2b39ea74cbce334c3999ae9141e796362d2cd8
Alexandru Ban <alexandru.ban.drmer@gmail.com>
2022-11-26 12:38:02 +0100

    Add example exercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{sync::{atomic::{AtomicU16, Ordering}, Arc}};
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("STARTING REST API SERVER");
+    let thread_counter = Arc::new(AtomicU16::new(1));
+    HttpServer::new(move || {
+        println!(
+            "Starting thread {}",
+            thread_counter.fetch_add(1, Ordering::SeqCst)
+        );
+        let thread_index = thread_counter.load(Ordering::SeqCst);
+        App::new()
+            .route("/", web::get().to(greet))
+            .route(
+                "/health",
+                web::get().to(move || {
+                    HttpResponse::Ok()
+                        .header("thread-id", thread_index.to_string())
+                        .finish()
+                }),
+            )
+            .route("/str", web::get().to(|| async { "Hola Rust" }))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
